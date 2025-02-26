d56c386913c5c9ff95ed985ebac034697f1e224f
github personal <humbertpt14@gmail.com>
2025-02-26 14:31:43 -0500

    fix(modalidad-facturacion): Cambio del nombre de ruta a modalidad-facturacion

--- app/routes/modalidad_facturacion_routes.py
+++ app/routes/modalidad_facturacion_routes.py
@@ -5,7 +5,7 @@ from app.services.modalidad_facturacion_service import verificar_medidor
 
 router = APIRouter()
 
-@router.get("/medidor-existe/{numero_conexion}")
+@router.get("/modalidad-facturacion/{numero_conexion}")
 def get_medidor(numero_conexion: str):
     """Devuelve 1 si el código de conexión tiene medidor, 0 si no tiene."""
     result = verificar_medidor(numero_conexion)
